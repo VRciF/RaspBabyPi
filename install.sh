179dff21786ce82b632381df11b6ddfdf106e80e
VRciF <prometheus__0@hotmail.com>
2017-06-13 23:04:36 +0200

    Create install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,25 @@ popd
 
 pushd /tmp
 
+#http://www.redhenlab.org/home/the-cognitive-core-research-topics-in-red-hen/the-barnyard/hardware-encoding-with-the-raspberry-pi
+#First activate the source repositories in /etc/apt/sources.list by adding this:
+#deb-src http://mirror.ox.ac.uk/sites/archive.raspbian.org/archive/raspbian/ jessie main contrib non-free rpi
+#deb-src http://archive.raspbian.org/raspbian/ jessie main contrib non-free rpi
+#deb-src http://www.deb-multimedia.org jessie main non-free
+#apt-get update
+#apt-get install git autoconf automake build-essential checkinstall libass-dev libgpac-dev libmp3lame-dev \
+#libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libspeex-dev libtheora-dev libtool libvorbis-dev \
+#pkg-config texi2html zlib1g-dev yasm dh-make fakeroot libfdk-aac-dev libx264-dev
+#cd ~/tmp
+#git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
+#cd fdk-aac
+#autoreconf -fiv
+#./configure --disable-shared
+#make
+#sudo checkinstall --pkgname=fdk-aac --pkgversion="$(date +%Y%m%d%H%M)-git" --backup=no --deldoc=yes --fstrans=no --default
+#mv fdk-aac_*-git-1_armhf.deb ~/packages/
+
+
 #echo "installing x264 codec"
 #git clone --depth 1 git://git.videolan.org/x264
 #cd x264
@@ -37,6 +56,7 @@ echo "installing ffmpeg"
 git clone --depth=1 git://source.ffmpeg.org/ffmpeg.git
 cd ffmpeg
 #./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-libfreetype
+#./configure --enable-gpl --enable-libx264 --enable-nonfree --enable-mmal --enable-omx --enable-omx-rpi --enable-libfdk-aac --enable-gpl --enable-libass --enable-libfdk-aac --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex --enable-librtmp --enable-libtheora --enable-libvorbis --enable-nonfree --enable-version3
 ./configure --arch=armel --target-os=linux --enable-gpl --enable-nonfree --enable-libfreetype --enable-libmp3lame
 time make -j4
 sudo make install
