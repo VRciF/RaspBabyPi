b5330be6cc79835e3e1ad16737a2195b2b484a9a
VRciF <prometheus__0@hotmail.com>
2017-06-18 15:50:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,12 +52,35 @@ pushd /tmp
 
 sudo apt-get install libmp3lame-dev
 
+git clone git://git.videolan.org/x264
+cd x264/
+./configure --enable-static --disable-opencl
+time make -j4 # 1m25.560
+checkinstall
+cd ..
+
+wget http://217.20.164.161/~tipok/aacplus/libaacplus-2.0.2.tar.gz
+tar -xzf libaacplus-2.0.2.tar.gz
+cd libaacplus-2.0.2
+./autogen.sh --with-parameter-expansion-string-replace-capable-shell=/bin/bash --host=arm-unknown-linux-gnueabi --enable-static --prefix=/my/path/were/i/keep/built/arm/stuff
+make -j4
+checkinstall
+cd ..
+
+wget http://mirrors.zerg.biz/alsa/lib/alsa-lib-1.0.25.tar.bz2
+tar xjf alsa-lib-1.0.25.tar.bz2
+cd alsa-lib-1.0.25/
+./configure --host=arm-unknown-linux-gnueabi --prefix=/my/path/were/i/keep/built/arm/stuff
+make -j4
+checkinstall
+cd ..
+
 echo "installing ffmpeg"
 git clone --depth=1 git://source.ffmpeg.org/ffmpeg.git
 cd ffmpeg
 #./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-libfreetype
 #./configure --enable-gpl --enable-libx264 --enable-nonfree --enable-mmal --enable-omx --enable-omx-rpi --enable-libfdk-aac --enable-gpl --enable-libass --enable-libfdk-aac --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex --enable-librtmp --enable-libtheora --enable-libvorbis --enable-nonfree --enable-version3
-#./configure --enable-gpl --enable-nonfree --enable-mmal --enable-omx --enable-omx-rpi --enable-gpl --enable-libass --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex --enable-librtmp --enable-libtheora --enable-libvorbis --enable-nonfree --enable-version3
+#./configure --enable-gpl --enable-nonfree --enable-mmal --enable-omx --enable-libaacplus --enable-libfreetype --enable-omx-rpi --enable-gpl --enable-libass --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex --enable-librtmp --enable-libtheora --enable-libvorbis --enable-nonfree --enable-version3
 ./configure --arch=armel --target-os=linux --enable-gpl --enable-nonfree --enable-libfreetype --enable-libmp3lame
 time make -j4
 sudo make install
